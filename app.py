09f0808219aea2c99eddc33570e19c9127c2f1c3
Sanjanarathod09 <rathodsanjana6668@gmail.com>
2024-10-02 20:04:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 from ultralytics import YOLO
 
 # Custom CSS for background image
-background_image_url = 'https://wallpapercave.com/background-hd-app'
+background_image_url = 'https://wallpapercave.com/wp/wp2855649.jpg'
 page_bg_img = f"""
 <style>
 .stApp {{
